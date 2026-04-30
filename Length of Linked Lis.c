d1ec9f13beff245514a1afff2004cf0872730e34
Keerthi-priya653 <sweetiesweetie69177@gmail.com>
2026-04-30 21:00:44 +0530

    Implement method to count nodes in linked list

---
+++ Length of Linked Lis.c
@@ -0,0 +1,19 @@
+using System;
+
+class Solution
+{
+    public int getCount(Node head)
+    {
+        int count = 0;
+
+        Node temp = head;
+
+        while (temp != null)
+        {
+            count++;
+            temp = temp.next;
+        }
+
+        return count;
+    }
+}
